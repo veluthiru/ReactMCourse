fd27d4a4483390b1e36a6dbe4a7cca40a50370b4
tvelu2 <tvelu2@visteon.com>
2026-02-09 19:34:27 +0530

    feat: add Header component with cart functionality and update CartContext

--- shopping-cart-ui/src/App.jsx
+++ shopping-cart-ui/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./component/Header";
 import ProductList from "./component/ProductList";
 
 const App = () => {
   return (
     <>
+      <Header />
       <div className="min-h-screen bg-gray-100 p-6">
         <h1 className="text-3xl font-bold mb-6"> Product Catalog</h1>
         <ProductList />
